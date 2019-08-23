ae636291744271875b2e43f032c391124df90a41
Nicola D'Oronzo <nicoladoronzo95@gmail.com>
2019-08-23 22:42:32 +0300

    SVG Icons refactor

--- src/Components/Icons/PlayIcon.jsx
+++ src/Components/Icons/PlayIcon.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const PlayIcon = ({ classIcon, dependencies, customFunction }) => (
-  <svg onClick={() => customFunction(dependencies)} className={classIcon} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+const PlayIcon = ({
+  className, ...props
+}) => (
+  <svg className={`icon ${className}`} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" {...props}>
     <path d="M3 22v-20l18 10-18 10z" />
   </svg>
 );
 
 PlayIcon.propTypes = {
-  classIcon: PropTypes.string.isRequired,
+  className: PropTypes.string,
   customFunction: PropTypes.func,
   dependencies: PropTypes.string,
 };
 
 PlayIcon.defaultProps = {
+  className: '',
   customFunction: () => {},
   dependencies: null,
 };
--- src/Components/Icons/TrashBinIcon.jsx
+++ src/Components/Icons/TrashBinIcon.jsx
@@ -2,40 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const TrashBinIcon = ({
-  classIcon, width, height, fill, iconTitle, dependencies, customFunction,
+  className, ...props
 }) => (
 
   <svg
-    className={classIcon}
+    className={`icon ${className}`}
     xmlns="http://www.w3.org/2000/svg"
-    width={width || 24}
-    height={height || 24}
+    width="24"
+    height="24"
     viewBox="0 0 24 24"
-    fill={fill || '#000'}
-    aria-labelledby={iconTitle}
-    onClick={() => customFunction(dependencies)}
+    {...props}
   >
-    <title id={iconTitle}>TrashBin Icon</title>
     <path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z" />
   </svg>
 );
 
 TrashBinIcon.propTypes = {
-  classIcon: PropTypes.string.isRequired,
-  width: PropTypes.number,
-  height: PropTypes.number,
-  fill: PropTypes.string,
-  iconTitle: PropTypes.string,
-  customFunction: PropTypes.func,
-  dependencies: PropTypes.string,
+  className: PropTypes.string,
 };
 
 TrashBinIcon.defaultProps = {
-  width: 24,
-  height: 24,
-  fill: '#fff',
-  iconTitle: 'trash bin',
-  customFunction: () => {},
-  dependencies: 'sth',
+  className: '',
 };
 export default TrashBinIcon;
