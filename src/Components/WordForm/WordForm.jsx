7494f7b2b537c2f656992bf258f4194edef35de4
Nicola D'Oronzo <nicoladoronzo95@gmail.com>
2019-09-04 19:59:11 +0300

    WordForm tested

--- src/Components/WordForm/WordForm.jsx
+++ src/Components/WordForm/WordForm.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './WordForm.css';
 import { useErrorContext } from '../../Contexts/ErrorContext';
+import PlusIcon from '../Icons/PlusIcon';
 
 
 const WordForm = ({ addWord }) => {
@@ -11,7 +12,7 @@ const WordForm = ({ addWord }) => {
   const createNewWord = (e) => {
     e.preventDefault();
     if (!newWord) {
-      handleError('Input something, please');
+      handleError('Add a word first');
     }
     addWord(newWord);
     setNewWord('');
@@ -24,38 +25,25 @@ const WordForm = ({ addWord }) => {
   };
 
   return (
-    <>
-      <form className="wordForm_container" onSubmit={e => createNewWord(e)}>
-        <AddWordInput handleChange={handleChange} newWord={newWord} />
-        <AddWordBtn />
-      </form>
-    </>
+    <form className="wordForm_container" onSubmit={e => createNewWord(e)}>
+      <input
+        className="wordForm_input"
+        type="text"
+        onChange={e => handleChange(e)}
+        value={newWord}
+        placeholder="Add word here..."
+      />
+      <PlusIcon disabled={!addWord} />
+    </form>
   );
 };
 
 WordForm.propTypes = {
-  addWord: PropTypes.func.isRequired,
+  addWord: PropTypes.func,
+};
+
+WordForm.defaultProps = {
+  addWord: null,
 };
 
 export default WordForm;
-
-const AddWordBtn = () => (
-  <button
-    className="wordForm_btn"
-    type="submit"
-  />
-);
-const AddWordInput = ({ handleChange, newWord }) => (
-  <input
-    className="wordForm_input"
-    type="text"
-    onChange={e => handleChange(e)}
-    value={newWord}
-    placeholder="Add word here..."
-  />
-);
-
-AddWordInput.propTypes = {
-  handleChange: PropTypes.func.isRequired,
-  newWord: PropTypes.string.isRequired,
-};
--- src/Components/WordForm/WordForm.test.js
+++ src/Components/WordForm/WordForm.test.js
@@ -1,12 +1,110 @@
 import React from 'react';
-import { create } from 'react-test-renderer';
+import { mount, shallow } from 'enzyme';
+import * as ErrorContext from '../../Contexts/ErrorContext';
 import WordForm from './WordForm';
 
-const addWord = jest.fn();
-
 describe('WordForm Component', () => {
+  const handleError = jest.fn();
+  const useErrorContext = jest.spyOn(ErrorContext, 'useErrorContext');
+  useErrorContext.mockImplementation(() => [null, handleError]);
+
+  let mountedWordForm;
+
+  const wordForm = (props, isShallow = false) => {
+    const jsx = (<WordForm {...props} />);
+    if (isShallow) return shallow(jsx);
+    return mount(jsx);
+  };
+
+  const props = {
+    addWord: null,
+  };
+
+  beforeEach(() => {
+    mountedWordForm = undefined;
+    jest.clearAllMocks();
+  });
+
   it('Matches to snapshot', () => {
-    const component = create(<WordForm addWord={addWord} />);
-    expect(component.toJSON()).toMatchSnapshot();
+    const component = wordForm(props, true);
+    expect(component).toMatchSnapshot();
+  });
+
+  describe('always', () => {
+
+    beforeEach(() => {
+      mountedWordForm = wordForm(props);
+    });
+
+    it('renders a form container', () => {
+      const formContainer = mountedWordForm.find('.wordForm_container');
+      expect(formContainer.exists()).toEqual(true);
+    });
+
+    it('renders an input', () => {
+      const addWordInput = mountedWordForm.find('input.wordForm_input');
+      expect(addWordInput.exists()).toEqual(true);
+    });
+
+    it('renders a PlusIcon button', () => {
+      const plusIcon = mountedWordForm.find('PlusIcon');
+      expect(plusIcon.exists()).toEqual(true);
+    });
+
+    it('disables the button if no addWord prop is passed', () => {
+      const plusIcon = mountedWordForm.find('PlusIcon');
+      expect(plusIcon.props().disabled).toEqual(true);
+    });
+  });
+
+  describe('when `addWord` is passed', () => {
+    beforeEach(() => {
+      props.addWord = jest.fn();
+      mountedWordForm = wordForm(props);
+    });
+
+    it('enables the the button', () => {
+      const plusIcon = mountedWordForm.find('PlusIcon');
+      expect(plusIcon.props().disabled).toEqual(false);
+    });
+
+    it('calls handleError with "Add a word first" if form is submitted', () => {
+      const plusIcon = mountedWordForm.find('PlusIcon');
+      plusIcon.simulate('submit');
+      expect(handleError).toHaveBeenCalledWith('Add a word first');
+    });
+
+    describe('when inputting', () => {
+      const testInput = 'Hello Input';
+      beforeEach(() => {
+        const input = mountedWordForm.find('.wordForm_input');
+        input.simulate('change', { target: { value: testInput } });
+      });
+
+      it('calls handleError with empty string inputting', () => {
+        expect(handleError).toHaveBeenCalledWith('');
+      });
+
+      it('changes the value of the input', () => {
+        const input = mountedWordForm.find('.wordForm_input');
+        expect(input.props().value).toEqual(testInput);
+      });
+
+      describe('when submitting', () => {
+        beforeEach(() => {
+          const plusIcon = mountedWordForm.find('PlusIcon');
+          plusIcon.simulate('submit');
+        });
+
+        it('calls addWord with the input inserted', () => {
+          expect(props.addWord).toHaveBeenCalledWith(testInput);
+        });
+
+        it('empties the input', () => {
+          const input = mountedWordForm.find('.wordForm_input');
+          expect(input.props().value).toEqual('');
+        });
+      });
+    });
   });
 });
