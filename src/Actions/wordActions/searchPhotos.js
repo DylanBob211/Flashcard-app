b1d1ed38310106d8c52ae0bda7eefb9fb4d84d86
DylanBob211 <47843313+DylanBob211@users.noreply.github.com>
2019-09-15 17:20:10 +0300

    fix api_key

--- src/Actions/wordActions/searchPhotos.js
+++ src/Actions/wordActions/searchPhotos.js
@@ -2,7 +2,7 @@ import Unsplash from 'unsplash-js';
 import searchPhotosFactory from './searchPhotos.factory';
 
 const unsplash = new Unsplash({
-  applicationId: '66c8f4bbdcd250ba88db15412fb4cf728a9a8c4db261db2a356b22a1365aa20a',
+  applicationId: 'YOUR_API_KEY',
 });
 
 export default searchPhotosFactory(unsplash);
